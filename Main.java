dc0e96dbcfe3bd4d8fbadebe40ab1af1ef62e57d
topgirl2410 <maria.topgirl82@gmail.com>
2022-12-18 16:09:17 +0100

    ejecicios echos

--- Main.java
+++ Main.java
@@ -17,3 +17,4 @@ class Coche {
         this.puertas++;
     }
 }
+
---
+++ Main1.java
@@ -0,0 +1,11 @@
+
+public class Main1 {
+    public static void main(String[] args) {
+        suma(10, 20, 15);
+        System.out.println();
+    }
+
+    public static void suma(int a, int b, int c) {
+        System.out.println(a + b + c);
+    }
+}
