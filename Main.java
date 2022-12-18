6c5bea4bad7d22ead6e97d4866e3102a3bc4e8a7
topgirl2410 <maria.topgirl82@gmail.com>
2022-12-18 15:56:36 +0100

    first commit

---
+++ Main.java
@@ -0,0 +1,19 @@
+// Ejercicio 2
+
+public class Main {
+    public static void main(String[] args) {
+        Coche miCoche = new Coche();
+        miCoche.AñadirPuertas();
+
+        System.out.println(miCoche.puertas);
+    }
+
+}
+
+class Coche {
+    public int puertas = 0;
+
+    public void AñadirPuertas() {
+        this.puertas++;
+    }
+}
